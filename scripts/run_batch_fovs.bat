9287aa3c10ac57c2d83a47d1a32a3997121543a5
LilianHeGao <lilianhegao@gmail.com>
2026-02-09 13:53:19 -0800

    ctrl 67

--- scripts/run_batch_fovs.bat
+++ scripts/run_batch_fovs.bat
@@ -5,7 +5,7 @@ REM Usage: run_batch_fovs.bat
 set DATA_FLD=\\192.168.0.116\durian3\Lilian\022425_FTD_smFISH_MBP_NRGN\coverslip1_controls\MBP_NRGN_set5
 set CACHE_ROOT=\\192.168.0.116\durian3\Lilian\merfish_sqi_cache
 set OUT_ROOT=output\022425_FTD_smFISH_MBP_NRGN\coverslip1_controls\MBP_NRGN_set5
-set ZARR_BASE=\\192.168.0.116\durian3\Lilian\022425_FTD_smFISH_MBP_NRGN\coverslip1_controls\MBP_NRGN_set5
+set ZARR_BASE=%DATA_FLD%
 set FOV_LIST=20 40
 
 for %%F in (%FOV_LIST%) do (
@@ -13,7 +13,7 @@ for %%F in (%FOV_LIST%) do (
     echo Processing FOV %%F ...
     echo ============================================================
     python scripts\run_sqi_from_fov_zarr.py ^
-        --fov_zarr   "%ZARR_BASE%\Conv_zscan1_%%F.zarr" ^
+        --fov_zarr   "%ZARR_BASE%\Conv_zscan5_%%F.zarr" ^
         --data_fld   "%DATA_FLD%" ^
         --cache_root "%CACHE_ROOT%" ^
         --out_root   "%OUT_ROOT%"
